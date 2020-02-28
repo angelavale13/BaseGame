95272cfec2ea11726ccbd7a4fc15ffa73db42039
angelavale13 <gypsy.aav@gmail.com>
2020-02-27 17:15:03 -0700

    update gravity and width

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let game;
 
 let gameOptions = {
-  gravity: 1,
+  gravity: 10,
   maxItemsPerLevel: 30,
   maxIterations: 10,
   minItemsDistance: 160
@@ -20,7 +20,7 @@ window.onload = function() {
       mode: Phaser.Scale.FIT,
       autoCenter: Phaser.Scale.CENTER_BOTH,
       parent: "thegame",
-      width: 750,
+      width: 1500,
       height: 1334
     },
     
